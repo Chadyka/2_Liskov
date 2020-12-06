0cbf25d43d6b6de1687a78ed21b85c61f6d8a5a0
chadyka <adamjakab13@gmail.com>
2020-12-06 23:18:43 +0100

    Second chapter, first two exercises

--- szulo_gyerek/Parent_Child/Main.java
+++ szulo_gyerek/Parent_Child/Main.java
@@ -3,10 +3,10 @@ package szulo_gyerek.Parent_Child;
 public class Main {
     public static void main(String[] args) {
         Parent s = new Child();
-        s.setName("Mother");
+        s.setName("Parent");
         s.setAge(60);
         Child gy = new Child();
-        s.setName("Laci");
+        s.setName("Child");
         s.setAge(15);
         System.out.println(gy.getName() + "  " + s.getName());
     }
